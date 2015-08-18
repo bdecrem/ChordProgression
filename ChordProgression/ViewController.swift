317b0b474694ecd71805b1dd6a574d84e8320c11
Bart Decrem <bdecrem@gmail.com>
2015-08-18 10:25:05 -0700

    Initial Commit

---
+++ ChordProgression/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChordProgression
+//
+//  Created by Bart Decrem on 8/18/15.
+//  Copyright © 2015 Bart Decrem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
