e64bf814ac403676b3d5231fe97684a0fdcacaae
bdecrem <bdecrem@gmail.com>
2015-08-18 17:14:51 -0700

    play a sound file. Swift 2.0 voodoo, using DO - CATCH

--- ChordProgression/ViewController.swift
+++ ChordProgression/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
+
+var chord:AVAudioPlayer = AVAudioPlayer()
+
 
 class ViewController: UIViewController {
 
+    @IBAction func playChord(sender: AnyObject) {
+    
+        let chordURL:NSURL = NSBundle.mainBundle().URLForResource("1", withExtension: "wav")!
+        
+        do {
+          
+            chord = try AVAudioPlayer(contentsOfURL: chordURL)
+            
+            
+            chord.numberOfLoops = 1
+            chord.prepareToPlay()
+            chord.play()
+
+            
+            
+        } catch _ {
+            return
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+       
     }
 
     override func didReceiveMemoryWarning() {
