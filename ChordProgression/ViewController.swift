95d53fdef8ff66b735a6f3f20d2b2797cbfc9429
bdecrem <bdecrem@gmail.com>
2015-08-18 18:37:03 -0700

    added array of 7 chords.  play random chord progression. display what you played (all ghetto).

--- ChordProgression/ViewController.swift
+++ ChordProgression/ViewController.swift
@@ -9,23 +9,40 @@
 import UIKit
 import AVFoundation
 
+
+
 var chord:AVAudioPlayer = AVAudioPlayer()
 
 
+
+let chordList1 = [ [1, "D", "I-IV-V", "D-G-A"], [2, "D", "I-V-I", "D-A-D"], [3, "D", "I-V-IV", "D-A-G"], [4, "G", "I-IV-V", "G-C-D"], [5, "G", "I-V-IV", "G-D-C"], [6, "G", "IV-I-V", "C-G-D"], [7, "G", "IV-V-I", "C-D-G"], [8, "G", "V-I-IV", "D-G-C"]]
+
 class ViewController: UIViewController {
 
-    @IBAction func playChord(sender: AnyObject) {
+    @IBOutlet var key: UILabel!
+    @IBOutlet var progression: UILabel!
+    @IBOutlet var chordsPlayed: UILabel!
     
-        let chordURL:NSURL = NSBundle.mainBundle().URLForResource("1", withExtension: "wav")!
+    @IBAction func playChord(sender: AnyObject) {
+        
+        let i = Int(arc4random_uniform(7))
+        let j = i + 1
+    
+        let chordURL:NSURL = NSBundle.mainBundle().URLForResource("\(j)", withExtension: "wav")!
         
         do {
           
             chord = try AVAudioPlayer(contentsOfURL: chordURL)
             
             
-            chord.numberOfLoops = 1
+            chord.numberOfLoops = 0
             chord.prepareToPlay()
             chord.play()
+            
+            key.text = "\(chordList1[i])"
+            progression.text = " "
+            chordsPlayed.text = " "
+            
 
             
             
