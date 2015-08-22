e256be1f2572ab84a0d0b95388f2dcc69eda2113
bdecrem <bdecrem@gmail.com>
2015-08-21 18:52:46 -0700

    Rileyfied

--- ChordProgression/ViewController.swift
+++ ChordProgression/ViewController.swift
@@ -15,74 +15,74 @@ let i = Int(arc4random_uniform(8))
 let chordList1 = [[0, "G", "V-I-IV", "D-G-C", 5140], [1, "D", "I-IV-V", "D-G-A", 1450], [2, "D", "I-V-I", "D-A-D", 1510], [3, "D", "I-V-IV", "D-A-G", 1540], [4, "G", "I-IV-V", "G-C-D", 1450], [5, "G", "I-V-IV", "G-D-C", 1540], [6, "G", "IV-I-V", "C-G-D", 4150], [7, "G", "IV-V-I", "C-D-G", 4510]]
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var key: UILabel!
     @IBOutlet var progression: UILabel!
     @IBOutlet var chordsPlayed: UILabel!
-    @IBOutlet var userGuess: UITextField?
+    @IBOutlet var userGuess: UITextField!
     @IBOutlet var correctIncorrect: UILabel!
-   
+    
     
     @IBAction func playChord(sender: AnyObject) {
-            print(i)
-    
-		if let chordURL = NSBundle.mainBundle().URLForResource("\(i)", withExtension: "wav") {
-			
-			do {
-			  
-				chord = try AVAudioPlayer(contentsOfURL: chordURL)
-				
-				
-				chord.numberOfLoops = 0
-				chord.play()
-				
-				
-			
-				
-			} catch _ {
-				return
-			}
-			
-		} else {
-			print("Unable to find audio file")
-		}
-		
+        print(i)
+        
+        if let chordURL = NSBundle.mainBundle().URLForResource("\(i)", withExtension: "wav") {
+            
+            do {
+                
+                chord = try AVAudioPlayer(contentsOfURL: chordURL)
+                
+                
+                chord.numberOfLoops = 0
+                chord.play()
+                
+                
+                
+                
+            } catch _ {
+                return
+            }
+            
+        } else {
+            print("Unable to find audio file")
+        }
+        
     }
     
     @IBAction func submitGuess(sender: AnyObject) {
         print(i)
         print(chordList1[i][4])
-        if userGuess != "" {
-            let userGuessInt:Int = Int((userGuess!.text)!)!
-        
+        if userGuess.text != "" {
+            let userGuessInt = Int(userGuess.text!)
+            
             if userGuessInt == chordList1[i][4] {
-            correctIncorrect.text = "Correct!"
+                correctIncorrect.text = "Correct!"
             } else {
-            correctIncorrect.text = ":("
+                correctIncorrect.text = ":("
             }
-       
+            
             key.text = "Key of: \(chordList1[i][1])"
             progression.text = "Type of Progression: \(chordList1[i][2])"
             chordsPlayed.text = "Chords Played: \(chordList1[i][3])"
             
-            } else {
-                correctIncorrect.text = "Please enter a guess"
-            }
-        
+        } else {
+            correctIncorrect.text = "Please enter a guess"
         }
+        
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
