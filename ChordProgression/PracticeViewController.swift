b2dd46f908303f8af37fe68c2d3b2dd4bb3cd707
Bart Decrem <bdecrem@gmail.com>
2015-09-01 19:33:29 -1000

    set up the practice view controller

--- ChordProgression/PracticeViewController.swift
+++ ChordProgression/PracticeViewController.swift
@@ -6,6 +6,38 @@
 //  Copyright © 2015 Bart Decrem. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import AVFoundation
+
+class PracticeViewController: UIViewController {
+
+    @IBAction func practice145(sender: AnyObject) {
+    print("practice I-IV-V")
+    
+    }
+    
+    @IBAction func practice154(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func practice415(sender: AnyObject) {
+    }
+    
+    @IBAction func practice451(sender: AnyObject) {
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
 
